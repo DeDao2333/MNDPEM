b275411ec440256d059346391e1a7424f9aa7f05
dedao2333 <595750464@qq.com>
2020-12-31 14:13:02 +0800

    add config for link and node-label color

---
+++ model/conf.py
@@ -0,0 +1,13 @@
+
+LINK_COLORs = {
+    'exist': '#BFBFBF',
+    'removed': '#1C86EE',
+    'predict_true': '#00CD66',
+    'predict_wrong': '#CD3700'
+}
+
+NODE_LABELs = {
+    1: '#DDB822',
+    2: '#44BBBB',
+    3: '#B87CC7'
+}
